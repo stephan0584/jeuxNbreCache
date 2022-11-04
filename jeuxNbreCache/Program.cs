a9c3553d1a3206a80fd3a932126dceb3c15816ae
stephan0584 <juaude@wanadoo.fr>
2022-11-04 18:19:39 +0100

    Ajoutez des fichiers projet.

---
+++ jeuxNbreCache/Program.cs
@@ -0,0 +1,78 @@
+﻿/**
+ * jeux du nombre caché
+ * auteur:
+ * date:
+ */
+ using System;
+
+namespace jeuxNbreCache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valeur=0,essai=0,nbre=1;
+            bool correct = false;
+            while (!correct)
+            {
+                try
+                {
+                    Console.Write("entrez un nombre:   ");
+                    valeur = int.Parse(Console.ReadLine());
+                    correct = true;
+                    
+                }
+                catch
+                {
+                    Console.WriteLine("erreur de saisie, recommencez");
+                }
+                
+            }
+            Console.Clear();
+            correct = false;
+            while (!correct)
+            {
+                try
+                {
+                    Console.Write("Entrez un essai:    ");
+                    essai = int.Parse(Console.ReadLine());
+                    correct = true;
+                }
+                catch
+                {
+                    Console.WriteLine("erreur de saisie, recommencez");
+                }
+            }
+            while (essai != valeur)
+            {
+               if (essai > valeur)
+                {
+                    Console.WriteLine("Trop grand");
+                }
+                else
+                {
+                    Console.WriteLine("Trop petit");
+                }
+                correct = false;
+                while (!correct)
+                {
+                    try
+                    {
+
+                        Console.Write("Entrez un essai:    ");
+                        essai = int.Parse(Console.ReadLine());
+                        correct = true;
+                        nbre++;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("erreur de saisie, recommencez");
+                    }
+                }
+            }
+            Console.WriteLine("Vous avez trouvé la valeur en "+nbre+"  fois");
+            Console.ReadLine();
+
+        }
+    }
+}
